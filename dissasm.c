304e661641a1083381f104c560f5fcba9a55bf91
icyjiub <tlneondo@gmail.com>
2022-04-04 17:47:03 -0400

    Impletmented 8

--- dissasm.c
+++ dissasm.c
@@ -32,6 +32,7 @@ void Disassemble(unsigned char *codebuffer, int pc){
 
     unsigned char *code = &codebuffer[pc];
     unsigned char firstNib = (code[0] >> 4);
+    unsigned char secNib = (code[1]);
     printf("%04x %02x %02x ", pc, code[0], code[1]);
     printf(" - ");
 
@@ -42,7 +43,18 @@ void Disassemble(unsigned char *codebuffer, int pc){
     switch(firstNib){
         case 0x00: 
             {
-                printf("0 not handled yet"); 
+                switch(secNib)
+                {
+                    case 0xE0:
+                    {
+                        printf("Display Clear - disp_clear() Call");
+                    }
+
+                    case 0xEE:
+                    {
+                        printf("Return");
+                    }
+                }
             }
             break;    
         case 0x01: //JUMP TO NNN ADDR
@@ -94,9 +106,41 @@ void Disassemble(unsigned char *codebuffer, int pc){
                 printf("%5s V%01X,#$%02x", "ADD", reg, code[1]);
             }
             break; 
-        case 0x08: // 9 Cases
+        case 0x08: // 9 Cases, involing X&Y registers
             {
-                printf("8 not handled yet");    
+                unsigned char regX = code[0] & 0x0f;
+                unsigned char regY = (code[1] & 0xf0) >> 4;
+                unsigned char optionNum = (code[1] & 0x0f);
+                switch(optionNum)
+                {
+                    case 0x00: // set vx to value in vy
+                        printf("%6s - V%d to V%d", "Assign", regX, regY);
+                        break;
+                    case 0x01: //vx = (VX | VY)
+                        printf("%6s - V%d = V%d OR V%d", "OR", regX, regX, regY);
+                        break;                    
+                    case 0x02: //vx = (VX & VY)
+                        printf("%6s - V%d = V%d AND V%d", "AND", regX, regX, regY);                    
+                        break;
+                    case 0x03: //vx = (VX XOR VY)
+                    printf("%6s - V%d = V%d XOR V%d", "XOR", regX, regX, regY);
+                        break;
+                    case 0x04: //VX += VY, VF set to 1 if carry
+                    printf("%s - V%d = V%d += V%d", "PLUSEQ - Const", regX, regX, regY);
+                        break;
+                    case 0x05: //VX -= VY, VF = 0 when borrow, 1 when not
+                    printf("%s - V%d = V%d -= V%d", "MINUSEQ - Const", regX, regX, regY);
+                        break;
+                    case 0x06: //VX >>= 1, send LSB to VF, then shift VX right by 1
+                    printf("%s - V%d >> 1", "SHIFTRIGHT", regX);
+                        break;
+                    case 0x07: //vx = Vy - Vx, VF = 0 when borrow, 1 if not
+                    printf("%s - V%d = V%d - V%d", "EQUALSDIFF", regX, regY, regX);                    
+                        break;
+                    case 0x0E: //Vx <<= 1, store MSB of VX in VF, then LShift VX by 1
+                        printf("%s - V%d >> 1", "SHIFTLEFT", regX);
+                        break;
+                }
             }
             break; 
         case 0x09: //9xY0 -- If (Vx != Vy) Skips the next instruction if VX does not equal VY
@@ -113,7 +157,10 @@ void Disassemble(unsigned char *codebuffer, int pc){
         case 0x0a: //ANNN - MEM Set I to the Addr NNN
             {    
                 unsigned char addresshi = code[0] & 0x0f;
-                printf("%5s I,#$%01x%02x", "MVI", addresshi, code[1]);
+                unsigned int addr = 0;
+                addr |= (addresshi << 8);
+                addr |= code[1];
+                printf("%5s I,0x%02x", "MVI", addr);
                  
             }    
             break;    
@@ -128,7 +175,7 @@ void Disassemble(unsigned char *codebuffer, int pc){
                 addrJMP |= (jmpOffseta << 8);
                 addrJMP |= jmpOffsetb;
 
-                printf("%5s PC = V0 + 0x%02x", "FLOW", addrJMP);
+                printf("%4s PC = V0 + 0x%02x", "FLOW", addrJMP);
             }
             break;
         case 0x0c: //CXNN Rand - Vx = rand() & NN -- Set Vx to the bit& of random number and NN
@@ -141,12 +188,12 @@ void Disassemble(unsigned char *codebuffer, int pc){
                 printf("d not handled yet");
             }
             break;
-        case 0x0e:
+        case 0x0e: // 2 cases
             {
                 printf("e not handled yet");
             }
             break;    
-        case 0x0f:
+        case 0x0f: // 9 cases
             {
                 printf("f not handled yet");
             }
